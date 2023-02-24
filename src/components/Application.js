d2c40f7e3e60555072e639a7028450f3e5c188b8
Colton Hyland <colthyland@gmail.com>
2023-02-24 10:25:55 -0800

    DayList added to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "components/Application.scss";
+import DayList from "components/DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -10,17 +33,23 @@ export default function Application(props) {
           className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
-        />
+        />8888
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu"><DayList
+          days={days}
+          day={day}
+          setDay={setDay}
+        /></nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
+        
       </section>
+      
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        
       </section>
     </main>
   );
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,18 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
+
+  const listDays = props.days.map((num) =>
+  <DayListItem 
+        key={num.id}
+        name={num.name} 
+        spots={num.spots} 
+        selected={num.name === props.day}
+        setDay={props.setDay}  
+      />
+  ); 
+
   return (
-    <ul></ul>
+    <ul>{listDays}</ul>
   );
 }
