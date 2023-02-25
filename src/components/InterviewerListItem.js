1fd0455b9483ddaaba92e272d4152b76c7f3123a
Colton Hyland <colthyland@gmail.com>
2023-02-25 12:56:20 -0800

    InterviewerListItem componented created

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import className from 'classnames';
+import 'components/InterviewerItemList.scss';
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = className("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+    "interviewers__item-image": props.avatar,
+    "interviewers__item--selected-image": props.selected
+  });
+
+  const displayName = (name) => {
+    if (props.selected) {
+      return name;
+    }
+  }
+
+  return (
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {displayName(props.name)}
+    </li>
+  );
+
+}
