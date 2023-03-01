dc34184b3bea96ac9ace6be9a233a72719d07758
Colton Hyland <colthyland@gmail.com>
2023-03-01 15:17:33 -0800

    useVisualMode handles transition and back to navigate states

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+//a Hook that allows our Appointment components to keep track of the "mode" that they are in as a user interacts with them to do things like create a new appointment, delete an appointment, etc
+
+export default function useVisualMode(initMode) {
+
+  //take an initial argument to set the mode state
+  //history array to the useVisualMode hook
+  const [history, setHistory] = useState([initMode]);
+
+  //take in a new mode and update the mode state with the new value
+  const transition = (newMode, replace = false) => {
+    if(replace) {
+      setHistory((prev) => [...prev.slice(0, -1), newMode]);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
+  };
+
+  //transition back to our previous mode
+  const back = () => {
+    //history limit condition
+    if(history.length < 2) {
+      return;
+    }
+    //history is reduced by the current mode
+    setHistory((prev) => {
+      const newHistory = [...prev];
+      newHistory.pop();
+      return newHistory;
+    });
+  };
+
+  const mode = history.slice(-1)[0];
+
+  return { mode, transition, back };
+}
